fff9a51d22a82928473e6d12a79f9b93fb833e40
Danicon <dancon.alferes@gmail.com>
2021-02-18 17:01:29 -0300

    Uma parte do projeto 10 realizado

--- Prj10/ex00/script.js
+++ Prj10/ex00/script.js
@@ -1,12 +1,30 @@
-function conte() {
-    var ini = Number(document.getElementById('ini').value)
-    var fim = Number(document.getElementById('fim').value)
-    var pas = Number(document.getElementById('pas').value)
-    var res = document.getElementById('res')
-    res.innerHTML = '<p></p>'
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    for (var c = ini; c<=fim; c+=pas) {
-        res.innerHTML += `${c} -> `
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando... </br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for(let c = i; c >= f; c-= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
-    res.innerHTML += 'Fim'
 }
